50b042734cd6a1ddcadb8afb0e82564589eebff3
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-28 15:22:03 +0200

    observable - html text

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 //This is called "viewmodel".This javascript section defines the data and
 //behavior of UI
+/*
+function AppViewModel() {
+    this.firstString = ko.observable("Enter First String");
+    this.secondString = ko.observable("Enter Second String");
 
-    function AppViewModel() {
-        this.firstString = ko.observable("Enter First String");
-        this.secondString = ko.observable("Enter Second String");
+    this.thirdString = ko.computed(function () {
+        return this.firstString() + " " + this.secondString();
+    }, this);
+}*/
 
-        this.thirdString = ko.computed(function () {
-            return this.firstString() + " " + this.secondString();
-        }, this);
-    }
+
+//Observables
+function AppViewModel(){
+    this.yourName = ko.observable("");
+    this.yourHTML = ko.observable("");
+}
+
+
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
+
+
+
+
