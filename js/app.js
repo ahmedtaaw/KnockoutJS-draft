44d97cc4d3b1c5da21464a27dfaa56b25b0e4d12
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-28 15:37:57 +0200

    option binding

--- js/app.js
+++ js/app.js
@@ -12,15 +12,30 @@ function AppViewModel() {
 
 
 //Observables
-function AppViewModel(){
+/*function AppViewModel(){
     this.yourName = ko.observable("");
     this.yourHTML = ko.observable("");
-}
 
+    this.availableCountries = ko.observableArray(['France', 'Germany', 'Spain'])
+}*/
 
+var Country = function(name, population) {
+    this.countryName = name;
+    this.countryPopulation = population;
+};
+
+var viewModel = {
+    availableCountries : ko.observableArray([
+        new Country("UK", 65000000),
+        new Country("USA", 320000000),
+        new Country("Sweden", 29000000)
+    ]),
+    selectedCountry : ko.observable() // Nothing selected by default
+};
+ko.applyBindings(viewModel);
 
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
+//ko.applyBindings(new AppViewModel());
 
 
 
